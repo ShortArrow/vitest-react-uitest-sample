529dcc9259a6e0fc2521d27f142c3c034eaf6b06
ShortArrow <bamboogeneral@shortarrow.jp>
2023-02-07 11:14:31 +0900

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,16 @@
 import react from "eslint-plugin-react";
-import reacthooks from "eslint-plugin-react-hooks";
+import reactHooks from "eslint-plugin-react-hooks";
+import testingLibrary from "eslint-plugin-testing-library";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import typescriptEslintParser from "@typescript-eslint/parser";
+import jestDom from "eslint-plugin-jest-dom";
 import eslintImport from "eslint-plugin-import";
 import globals from "globals";
+
+const jsRules = {
+  semi: "error",
+};
+const tsRules = {};
 
 export default [
   "eslint:recommended",
@@ -17,27 +26,62 @@ export default [
     ignores: ["./node_modules/**/*"],
   },
   {
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
     rules: {
-      semi: "off",
+      ...jsRules,
     },
   },
   {
-    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: typescriptEslintParser,
+      parserOptions: {
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      typescriptEslint: typescriptEslint,
+    },
+    rules: {
+      ...jsRules,
+      ...tsRules,
+    },
+  },
+  {
+    files: ["tests/**/*.{ts,tsx}"],
+    plugins: {
+      testingLibrary: testingLibrary,
+      jestDom: jestDom,
+    },
+  },
+  {
+    files: ["**/*.cjs"],
+    languageOptions: {
+      parserOptions: {
+        sourceType: "commonjs",
+        project: "./tsconfig.json",
+      },
+    },
+  },
+  {
+    files: ["**/*.{jsx,tsx}"],
     plugins: {
       react: react,
-      "react-hooks": reacthooks,
+      reactHooks: reactHooks,
     },
     languageOptions: {
       parserOptions: {
-        ecmaVersion: "latest",
         sourceType: "module",
-        project:"./tsconfig.json",
+        project: "./tsconfig.json",
         ecmaFeatures: {
           jsx: true,
         },
-      },
-      globals: {
-        ...globals.browser,
       },
     },
     rules: {
